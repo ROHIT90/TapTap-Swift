653a4fd9466eadd6c55d494de7540e0dc34fb779
rohit FNU <rohitfnu@rohits-MacBook-Pro.local>
2016-03-18 17:04:26 -0400

    added functionality

--- TapTap/ViewController.swift
+++ TapTap/ViewController.swift
@@ -8,18 +8,110 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController,UITextFieldDelegate {
+    //outlets
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var howMnayTapsTextFiewld: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var tapButton: UIButton!
+    @IBOutlet weak var tabLabel: UILabel!
+    
+    
+    //variables
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        howMnayTapsTextFiewld.delegate = self
+        self.addDoneButtonOnKeyboard()
     }
+    
+    @IBAction func playButtonTouchUpInside(sender: UIButton!)
+    {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        if (howMnayTapsTextFiewld.text != nil && howMnayTapsTextFiewld.text != ""){
+            
+            logoImageView.hidden = true
+            playButton.hidden = true
+            howMnayTapsTextFiewld.hidden = true
+            
+            tapButton.hidden = false
+            tabLabel.hidden = false
+            
+            maxTaps = Int(howMnayTapsTextFiewld.text!)!
+            currentTaps = 0
+            
+            updateTapLabel()
+        }
     }
+    
+    @IBAction func tapButtonTouchUpInside (sender : UIButton!)
+    {
+        currentTaps++
+        updateTapLabel()
+    }
+    
+    func updateTapLabel()
+    {
+        tabLabel.text = "\(currentTaps) Taps"
 
-
+    }
+    
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func restartGame()
+    {
+        maxTaps = 0
+        howMnayTapsTextFiewld.text = ""
+        
+        logoImageView.hidden = false
+        playButton.hidden = false
+        howMnayTapsTextFiewld.hidden = false
+        
+        tapButton.hidden = true
+        tabLabel.hidden = true
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool
+    {
+    textField.resignFirstResponder()
+    return true
+    }
+    
+    func addDoneButtonOnKeyboard()
+    {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRectMake(0, 0, 320, 50))
+        doneToolbar.barStyle = UIBarStyle.BlackTranslucent
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Done, target: self, action: Selector("doneButtonAction"))
+        
+        let items = NSMutableArray()
+        items.addObject(flexSpace)
+        items.addObject(done)
+        
+        doneToolbar.items = [done]
+        doneToolbar.sizeToFit()
+        
+        self.howMnayTapsTextFiewld.inputAccessoryView = doneToolbar
+        self.howMnayTapsTextFiewld.inputAccessoryView = doneToolbar
+        
+    }
+    
+    func doneButtonAction()
+    {
+        self.howMnayTapsTextFiewld.resignFirstResponder()
+        self.howMnayTapsTextFiewld.resignFirstResponder()
+    }
 }
 
